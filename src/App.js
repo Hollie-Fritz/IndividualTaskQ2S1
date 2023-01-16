0e88e9304e4b366b463f094f0d5093a51ef0c7f3
Hollie Fritz <hollie.fritz@bellevuecollege.edu>
2023-01-16 13:05:29 -0800

    start of tutorial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+// functional component
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World
         </p>
         <a
           className="App-link"

7b930d47843f71684a8e09f57fc198c4ad0edf83
Hollie Fritz <hollie.fritz@bellevuecollege.edu>
2023-01-16 16:04:11 -0800

    useEffect after render

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css'
-import HookCounterFour from './components/HookCounterFour'
+import ClassCounterOne from './components/ClassCounterOne';
+import HookCounterOne from './components/HookCounterOne'
 
 
 function App() {
   return (
     <div className='App'>
-      <HookCounterFour />
+      <HookCounterOne/>
   </div>
   )
 }

52870781b619b1710d1e21e8d9a8d62f74285195
Hollie Fritz <hollie.fritz@bellevuecollege.edu>
2023-01-16 17:17:39 -0800

    fetching data with useEffect part 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css'
+import DataFetching from './components/DataFetching';
 import IntervalHookCounter from './components/IntervalHookCounter';
-import MouseContainer from './components/MouseContainer';
 
 
 function App() {
   return (
     <div className='App'>
-      <IntervalHookCounter/>
+      <DataFetching />
   </div>
   )
 }

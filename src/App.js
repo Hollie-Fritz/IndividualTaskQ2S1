1dcf73dc5744447fee96fcc4dd633a127ef7108b
Hollie Fritz <hollie.fritz@bellevuecollege.edu>
2023-01-16 16:37:39 -0800

    useEffect with cleanup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css'
 import ClassCounterOne from './components/ClassCounterOne';
 import HookCounterOne from './components/HookCounterOne'
 import HookMouse from './components/HookMouse';
+import MouseContainer from './components/MouseContainer';
 
 
 function App() {
   return (
     <div className='App'>
-      <HookMouse />
+      <MouseContainer/>
   </div>
   )
 }

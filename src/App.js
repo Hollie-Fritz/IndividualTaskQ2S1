7c62514c99bae3669c5f367798e8e68057ec9b1e
Hollie Fritz <hollie.fritz@bellevuecollege.edu>
2023-01-17 16:01:21 -0800

    datafetching using useState and useEffect hooks

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
 import React, {useReducer} from 'react'
 import './App.css'
-import ComponentA from './components/ComponentA'
-import ComponentB from './components/ComponentB'
-import ComponentC from './components/ComponentC'
+import DataFetchingOne from './components/DataFetchingOne';
 
-// to provide the values - create a context called CountContext
-export const CountContext = React.createContext()
-//define initial state
-const initialState = 0
-//accept current state and action then return new state
-//action is a instruction to the reducer function
-//define reducer function
-const reducer = (state, action) => {
-    switch(action){
-        case 'increment':
-            return state + 1
-        case 'decrement':
-            return state - 1
-        case 'reset':
-            return initialState
-        //unknown action - default
-        default:
-            return state
-    }
-}
 
 function App() {
-  // create counter using reducer hook
-  const [count, dispatch] = useReducer(reducer, initialState)
   return (
-    // pass countState and countDispatch as two parameters which returns the current state 
-    // value and dispatch method
-    // at the topmost level provide the two values as an object
-    <CountContext.Provider value = {{countState: count, countDispatch: dispatch}}> 
-    <div className='App'>
-      Count - {count}
-      <ComponentA />
-      <ComponentB />
-      <ComponentC />
+  <div className='App'>
+    <DataFetchingOne />
   </div>
-  </CountContext.Provider>
+
   )
 }
 
---
+++ src/components/DataFetchingOne.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+
+function DataFetchingOne() {
+    //loading flag
+    const [loading, setLoading] = useState(true)
+    //error message
+    const [error, setError] = useState('')
+    //hold the post from API endpoint
+    const [post, setPost] = useState({})
+    
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/posts/1')
+         .then(response => {
+            setLoading(false)
+            setPost(response.data)
+            setError('')
+         })
+         .catch(error => {
+            setLoading(false)
+            setPost({})
+            setError('something went wrong')
+         })
+    }, [])
+    //^^ empty dependency array
+    return (
+        <div>
+            {/* if busy loading the data then display 'loading' 
+            else display post title*/}
+            {loading ? 'loading' : post.title}
+            {/* if error display error, else return null */}
+            {error ? error : null}
+        </div>
+    )
+}
+
+export default DataFetchingOne;

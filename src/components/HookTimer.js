4d6f31b7a5de5c7483994bb1870039522b6757ca
Hollie Fritz <hollie.fritz@bellevuecollege.edu>
2023-01-17 19:28:22 -0800

    formatting fix

--- src/components/HookTimer.js
+++ src/components/HookTimer.js
@@ -1,26 +1,28 @@
-import React, {useState, useEffect, useRef} from 'react'
+import React, { useState, useEffect, useRef } from "react";
 
 function HookTimer() {
-    const[timer, setTimer] = useState(0)
-    const intervalRef = useRef()
+  const [timer, setTimer] = useState(0);
+  const intervalRef = useRef();
 
-    //set timer
-    //mimic the function of didMount and willUnmount
-    useEffect(() => {
-        intervalRef.current = setInterval(() => {
-            setTimer(prevTimer => prevTimer + 1)
-        }, 1000)
-        return () => {
-            // clean up
-            clearInterval(intervalRef.current)
-        }
-    }, [])
-    return (
-        <div>
-           Hook Timer - {timer} 
-           <button onClick={() => clearInterval(intervalRef.current)} >Clear Hook Timer</button>
-        </div>
-    )
+  //set timer
+  //mimic the function of didMount and willUnmount
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setTimer((prevTimer) => prevTimer + 1);
+    }, 1000);
+    return () => {
+      // clean up
+      clearInterval(intervalRef.current);
+    };
+  }, []);
+  return (
+    <div>
+      Hook Timer - {timer}
+      <button onClick={() => clearInterval(intervalRef.current)}>
+        Clear Hook Timer
+      </button>
+    </div>
+  );
 }
 
-export default HookTimer
+export default HookTimer;
